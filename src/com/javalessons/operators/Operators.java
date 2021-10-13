e1e061eadce4862771398a8d2b2fee2fe9c68ea1
timurio <timos.gooogle@gmail.com>
2021-10-13 21:56:16 +0500

    git init

---
+++ src/com/javalessons/operators/Operators.java
@@ -0,0 +1,22 @@
+package com.javalessons.operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        double a = 10;
+        double b = 3;
+
+        double c = a + b;
+        double d = a - b;
+
+        double e = a * b;
+        double f = a / b;
+
+        double g = a % b; // Остаток от деления
+
+        System.out.println("v= " + c);
+        System.out.println("d= " + d);
+        System.out.println("e= " + e);
+        System.out.println("f= " + f);
+        System.out.println("g= " + g);
+    }
+}
