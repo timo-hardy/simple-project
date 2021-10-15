f778affac40aba4958a560bb552ad3ff533d83d0
Timur <timos.gooogle@gmail.com>
2021-10-15 09:56:34 +0500

    Realization Control.java

--- src/com/javalessons/contrlolstatement/Control.java
+++ src/com/javalessons/contrlolstatement/Control.java
@@ -2,6 +2,15 @@ package com.javalessons.contrlolstatement;
 
 public class Control {
     public static void main(String[] args) {
-
+        int testScore = 50;
+        System.out.println("You have got");
+        if (testScore >= 90) {
+            System.out.println("a excellent");
+        } else if (testScore >= 75) {
+            System.out.println("a good");
+        } else if (testScore >= 60) {
+            System.out.println("a satisfactory");
+        }
+        System.out.println("mark");
     }
 }
